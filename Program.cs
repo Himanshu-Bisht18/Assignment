e993651de9abe4da0a8d4c21aa134d50c04346ac
Himanshu Bisht <104342913+Himanshu-Bisht18@users.noreply.github.com>
2022-05-20 10:29:06 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace reverseOrderofNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+
+            int[] numbers = new int[5];
+            int[] reverse = new int[5];
+            int i;
+            for (i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"Enter the number {i+1} : ");
+                numbers[i] = int.Parse(Console.ReadLine());
+            
+                while (numbers[i] > 0)
+                {
+                    reverse [i]= reverse[i] * 10+ numbers[i] % 10;
+                    numbers[i] = numbers[i] / 10;
+                }
+                    Console.WriteLine($"Reversed number : {reverse[i]}");
+                Console.WriteLine("");
+
+              
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
