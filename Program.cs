ece30209dd4bf08fc02eb247e4903f1ee9f5243a
Himanshu Bisht <104342913+Himanshu-Bisht18@users.noreply.github.com>
2022-05-21 01:31:03 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿
+using System;
+
+namespace abbreviatedFormat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            int i = 0;
+            Console.WriteLine("Enter any name: ");
+            name = Console.ReadLine();
+            char[] letters = name.ToCharArray();
+            Console.Write(letters[0] + ". ");
+            for (i = 0; i < letters.Length - 1; i++)
+            {
+                if ( letters[i] ==' ')
+                {
+                    Console.Write(letters[i + 1] + ". ");
+                }
+            }
+        
+                     
+
+           
+            
+            
+
+
+
+
+        }
+    }
+}
+        
+       
+
+            
+       
+
+
+      
+
+
+            
+
+
+
+
+      
+
+
+
+ 
+
