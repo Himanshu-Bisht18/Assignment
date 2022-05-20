a3ac195bd475a2e4d1c9093c103f4b3c816078af
Himanshu Bisht <104342913+Himanshu-Bisht18@users.noreply.github.com>
2022-05-20 10:51:38 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace sumOfElementsOfArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, sum = 0;
+            int[] numbers = new int[10];
+            for(i=0;i<10;i++)
+            {
+                Console.WriteLine($"Enter element {i+1} : ");
+                numbers[i] = int.Parse(Console.ReadLine());
+                Console.WriteLine("First stored number is : " +numbers[i]);
+                sum += numbers[i];
+            }
+            Console.WriteLine($"Sum of {i} stored in array is : {sum}");
+        }
+    }
+}
