62cf00d28c6f9308cef232d375cec56c2d77884a
Himanshu Bisht <104342913+Himanshu-Bisht18@users.noreply.github.com>
2022-05-21 09:11:57 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace reverseString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { string text;
+           
+            string reverse = String.Empty;
+            Console.WriteLine("enter a string");
+            text = Console.ReadLine();
+
+            for (int i = text.Length - 1; i >= 0; i--)
+            {
+                reverse += str[];
+            }
+
+            Console.WriteLine(reverse);
+
+            Console.ReadLine()
+
+
+        }
+    }
+}
