d2a7e09bb2ad3637401c04567b6f549227237a23
Himanshu Bisht <104342913+Himanshu-Bisht18@users.noreply.github.com>
2022-05-22 20:12:30 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace CountingwithStarsAtdiagonal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int col, row;
+            for(row=1; row<=5; row++)
+            {
+                for(col=1; col<=5; col++)
+                { if(col==row)
+                    {
+                        Console.Write("*"+" ");
+                    }
+                else
+                    {
+
+                    Console.Write(col + " ");
+                    }
+                }
+                Console.WriteLine(" ");
+            }
+          
+        }
+    }
+}
