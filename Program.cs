acd8165bf4afe098ae75d43e29e583e95780bd41
Himanshu Bisht <104342913+Himanshu-Bisht18@users.noreply.github.com>
2022-05-22 18:42:08 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { int col, row;
+            for (row = 1; row <= 4 ; row++)
+            {
+                for ( col =1; col<=row; col++)
+                {
+                 
+                     Console.Write(col + " ");
+                }
+                Console.WriteLine("");
+
+            }
+           
+        }
+         
+    }
+}
