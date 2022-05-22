7eba1e02f7bc2d874d9cf39ba88b320e8d24e1bf
Himanshu Bisht <104342913+Himanshu-Bisht18@users.noreply.github.com>
2022-05-22 19:25:04 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace starDecreasingPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int row, col;
+            for (row = 1; row <= 6; row++)
+            {
+                for (col = 6; col >= row; col--)
+                {
+                    Console.Write("*"+" ");
+
+                }
+                Console.WriteLine(" ");
+            }
+        }
+    }
+}
