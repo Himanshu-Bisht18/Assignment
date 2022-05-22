9dc600561c4450f9620feb38b619215abceb45c2
Himanshu Bisht <104342913+Himanshu-Bisht18@users.noreply.github.com>
2022-05-22 22:51:19 +0530

    Add files via upload
    Output:
    a b c d e
    a b c d
    a b c 
    a b 
    a

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace alphabetsDecrement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int row, col;
+            
+            for(row=5; row>=1; row--)
+            {   
+                for(col=1; col<=row;col++)
+                {
+                    Console.Write(Convert.ToChar(96+col)+" ");
+                }
+                Console.WriteLine(" ");
+            }
+        }
+    }
+}
