5954b6a399a71325d68126690a96329e5751c238
Himanshu Bisht <104342913+Himanshu-Bisht18@users.noreply.github.com>
2022-05-22 12:24:29 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace OccurenceOfNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[10];
+
+
+            for(int i=0;i<10;i++)
+
+            {
+                Console.WriteLine($"Enter {i+1} number of array : ");
+                numbers[i] = int.Parse(Console.ReadLine());
+               
+            }
+            Console.Write($"Numbers of array are :");
+            foreach (int n in numbers)
+            {
+                Console.WriteLine(n);
+                Console.Write(" ");
+            }
+
+            Console.WriteLine("Enter number to check occurrence  :  ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            int occurs = 0;
+            for (int j = 0; j<= numbers.Length - 1; j++)
+            {
+                if ( number== numbers[j] )
+                {
+                    occurs++;
+                }
+            }
+
+            Console.WriteLine($"occurrence of {number} in the given array is {occurs}");
+
+            Console.ReadKey();
+
+        }
+    }
+}
+
+
+
+
