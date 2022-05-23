af49c1a57a7732ec673e488e9f99b579186f120d
Himanshu Bisht <104342913+Himanshu-Bisht18@users.noreply.github.com>
2022-05-23 12:48:24 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace reverseOrderOfNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number,remainder, reverseNumber=0;
+            Console.Write("Enter any number to get reverse: ");
+            number = Convert.ToInt32(Console.ReadLine());
+            while (number != 0)
+            {
+                remainder = number % 10;
+                reverseNumber = reverseNumber * 10 + remainder;
+                number /= 10;
+            }
+            Console.Write("Reversed Number: " + reverseNumber);
+             
+             
+            
+            
+        }   
+    }
+}
