b5a380bffdf9a160bd914d5699cb1a5f0f5be78b
Himanshu Bisht <104342913+Himanshu-Bisht18@users.noreply.github.com>
2022-05-23 12:14:31 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace sort_3numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sorting of three numbers");
+            Console.WriteLine("Enter first number:");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter second number:");
+            int number2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter third number:");
+            int number3 = Convert.ToInt32(Console.ReadLine());
+
+            if (number1 == number2 || number1 == number3)
+            {
+                Console.WriteLine("Provide three different numbers");
+            }
+            else if (number1 < number2 && number2 < number3 && number2 < number3)
+            {
+                Console.WriteLine($"sorted order of numbers is:{number1},{number2},{number3}");
+            }
+
+
+            else if (number2 < number3 && number2 < number1 && number3 < number1)
+
+            {
+                Console.WriteLine($"sorted order of numbers is:{number2},{number3},{number1}");
+            }
+            else
+            {
+                Console.WriteLine($"sorted order of numbers is:{number3},{number1},{number2}");
+            }
+
+        }
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+        
+     
+ 
