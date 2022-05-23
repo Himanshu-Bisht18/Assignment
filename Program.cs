74fc1637cd8d8afe8efc15c3f182d3a55796faeb
Himanshu Bisht <104342913+Himanshu-Bisht18@users.noreply.github.com>
2022-05-23 12:17:28 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace smallest_number_among_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first number:");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter second number:");
+            int number2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter third number:");
+            int number3 = Convert.ToInt32(Console.ReadLine());
+            if (number1 < number2 && number1 < number3)
+            {
+                Console.WriteLine($"Among numbers given above, {number1} is the smallest");
+            }
+            else if (number2 < number3 && number2 < number1)
+            {
+                Console.WriteLine($"Among numbers given above, {number2} is the smallest");
+            }
+            else
+            {
+                Console.WriteLine($"Among numbers given above, {number3} is the smallest");
+
+            }
+            
+
+
+        }
+
+    }
+}
