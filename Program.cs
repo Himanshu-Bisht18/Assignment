e30dba4bdf42f83f52ede3ea96bc7eed93e8992b
Himanshu Bisht <104342913+Himanshu-Bisht18@users.noreply.github.com>
2022-05-23 12:46:58 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace reverseCounting4321
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int row, col;
+            for(row=1; row<=4;row++)
+            {
+                for (col=5-row; col>=1; col--)
+                {
+                    Console.Write(col+ " ");
+                }
+                Console.WriteLine(" ");
+            }
+
+        }
+    }
+}
